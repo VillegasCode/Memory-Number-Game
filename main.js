b7b12f65ea124251dac4f160feb0685ed5545b5e
Carlos Villegas <carlos.villegasch@gmail.com>
2022-11-20 09:37:05 -0500

    Function destapar created and variables tarjeta1 and tarjeta2 to compare results

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 //We created variables
 let tarjetasDestapadas = 0;
+let tarjeta1 = null;
+let tarjeta2 = null;
+let primerResultado = null;
+let segundoResultado = null;
 
 //Create a array with numbers
 let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
@@ -13,4 +17,14 @@ console.log(numeros);
 function destapar(id) {
     tarjetasDestapadas++;
     console.log(tarjetasDestapadas);
+
+    if (tarjetasDestapadas == 1){
+        //Show first number id
+        tarjeta1 = document.getElementById(id);
+        primerResultado = numeros[id]
+        tarjeta1.innerHTML = primerResultado;
+
+        //Disable the first button pushed
+        tarjeta1.disabled = true;
+    }
 }
